c056f3339eeac62dd22499d297df9247e30710ba
matosky <cyphercode59@gmail.com>
2023-03-05 14:31:07 +0100

    new

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
+const section = document.querySelector('#about');
 
-window.addEventListener('load', function () {
+
+window.addEventListener('load', async function () {
   let heroText = document.querySelector('#herotext');
     heroText.classList.add('animate');
-});
-
-window.addEventListener('load', function () {
-    let contact = document.querySelectorAll('.small-flex');
+     let contact = document.querySelectorAll('.small-flex');
     contact.forEach(item => {
         item.classList.add("animate2")
     })
 });
+
+
+const observer = new IntersectionObserver(entries => {
+    if (entries[0].isIntersecting) {
+      section.classList.add('animate3');
+      console.log(section);
+  } else {
+    section.classList.remove('animate3');
+  }
+});
+
+observer.observe(section);
+
